a5e7e55f024976095868d57fb202deb1809e7016
Giulio Zhou <giuliozhou8@gmail.com>
2016-08-22 19:35:54 -0700

    Added sequence reading functionality to make_param_txt

--- util/make_param_txt.py
+++ util/make_param_txt.py
@@ -11,9 +11,26 @@ parser.add_argument('--data_dir',
     help="Directory containing image data")
 parser.add_argument('--num_timesteps',
     help="Number of timesteps to compute tight mean/covariance")
-parser.add_argument('--query',
+parser.add_argument('--query_type',
     help="Query type")
 args = parser.parse_args()
+
+def save_img_txt():
+    data = []
+    for img_path in os.listdir(args.data_dir):
+        img = skio.imread(os.path.join(args.data_dir, img_path))
+        if len(img.shape) < 3:
+            img = np.dstack((img, img, img))
+        data.append(img)
+    data = np.array(data)
+    t, ylen, xlen, c = data.shape
+    colors = ['r', 'g', 'b']
+    for channel in range(c):
+        data_slice = data[:, :, :, channel].reshape((t, ylen*xlen))
+        np.savetxt(
+            os.path.join(args.input_dir,
+                         'data_{}.txt'.format(colors[channel])), data_slice)
+
 
 def get_tight_mean_var():
     data = []
@@ -31,7 +48,7 @@ def get_tight_mean_var():
     for x in range(xlen):
         print(x)
         for y in range(ylen):
-            H, edges = np.histogramdd(data[:, y, x], bins=(64,) * 3,
+            H, edges = np.histogramdd(data[:, y, x], bins=(24,) * 3,
                                       range=((0, 255), (0, 255), (0, 255)))
             bin_idx = np.unravel_index(np.argmax(H), H.shape)
             mean = np.array([(edges[i][bin_idx[i]] + edges[i][bin_idx[i] + 1]) \
@@ -44,8 +61,8 @@ def get_tight_mean_var():
             small_var[y, x] = cov
     return small_mean.reshape(xlen * ylen, c), small_var.flatten()
 
-def main():
-    # small_mean, small_var = get_tight_mean_var()
+
+def read_local_params():
     i = 0
     mean_arr, var_arr = [], []
     while True:
@@ -57,8 +74,16 @@ def main():
         mean_arr.append(mean.reshape(mean.shape[0] * mean.shape[1], mean.shape[2]))
         var_arr.append(var.flatten())
         i += 1
-    # mean_arr.append(small_mean)
-    # var_arr.append(small_var)
+    return mean_arr, var_arr
+
+
+def main():
+    mean_arr, var_arr = read_local_params()
+    if args.query_type == 'read_img_sequence':
+        small_mean, small_var = get_tight_mean_var()
+        mean_arr.append(small_mean)
+        var_arr.append(small_var)
+        save_img_txt()
     np.savetxt(os.path.join(args.input_dir, "means.txt"),
                np.vstack(mean_arr))
     np.savetxt(os.path.join(args.input_dir, "vars.txt"),
